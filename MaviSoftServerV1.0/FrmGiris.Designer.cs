9c4cccd668a7751cf145453daadcd26f285c2118
Coşkun KARAKOÇ <coskunkarakocc@gmail.com>
2020-01-23 16:23:29 +0300

    KeyPressEnter

--- MaviSoftServerV1.0/FrmGiris.Designer.cs
+++ MaviSoftServerV1.0/FrmGiris.Designer.cs
@@ -176,6 +176,7 @@
             // 
             // FrmGiris
             // 
+            this.AcceptButton = this.btnTamam;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(264, 261);
